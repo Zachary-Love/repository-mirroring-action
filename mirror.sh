60651a915b1c759eae39591e1579b2ff7dfcde24
Zachary-Love <56354999+Zachary-Love@users.noreply.github.com>
2024-01-26 20:10:22 -0600

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -10,15 +10,18 @@ export LFS_CHECK=$?
 # Check if the repository uses Git LFS
 if [ $LFS_CHECK -eq 0 ]; then
   echo "Found LFS Repository"
+  echo "$INPUT_TARGET_REPO_URL"
   # If it does, disable LFS locking verification for the mirror remote
   git config lfs."$INPUT_TARGET_REPO_URL"/info/lfs.locksverify false
 
   # Fetch and push all LFS files
+  echo "LFS Fetch"
   git lfs fetch --all
   git lfs push --all mirror
 fi
 
 # Push the Git history to the mirror repository
+echo "Pushing to mirror repository"
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
 
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
